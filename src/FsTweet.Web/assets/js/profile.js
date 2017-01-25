82b7d23944d740c353ab5c50c4df94e84d1385c0
Tamizhvendan S <tamizh88@gmail.com>
2017-01-25 17:37:33 +0530

    wrapping up tweet post

--- src/FsTweet.Web/assets/js/profile.js
+++ src/FsTweet.Web/assets/js/profile.js
@@ -1,18 +1,47 @@
 $(function(){
 
+  var tweetReadTmpl = `
+    {{#tweets}}
+      <div>        
+        <div class="tweet_read_view bg-info">
+          <span class="text-muted">@{{username.value}} - {{#timeAgo}}{{time}}{{/timeAgo}}</span>
+          <p>{{tweet.value}}</p>
+        </div>        
+      </div>
+
+    {{/tweets}}
+  `
+
+  var timeAgo = function () {
+    return function(val, render) {
+      return moment(render(val)).fromNow()
+    };
+  }
+
+  var username = window.location.pathname.substring(1)
+
   function loadTweets(username) {
     $.getJSON("/tweets/" + username, function(){
 
     }).done(function(data){
-      var template = $("#tweetReadTmpl").html()
-      Mustache.parse(template); 
-      var htmlOutput = Mustache.render(template, data);
+      if (data.length === 0) {
+        $("#tweets").html('<p class="bg-warning no_tweets">No tweets found <p></div>')
+        return
+      }
+      var htmlOutput = Mustache.render(tweetReadTmpl, {
+        "tweets" : data,
+        "timeAgo" : timeAgo
+      });
       $("#tweets").html(htmlOutput)
     }).fail(function(data){
       alert(data)
     });
   }
 
-  loadTweets(window.location.pathname.substring(1))
+  loadTweets(username)
+
+  $(document).on('tweet:posted', function(){
+    loadTweets(username)
+  })
 
 });
--- src/FsTweet.Web/assets/js/tweet.js
+++ src/FsTweet.Web/assets/js/tweet.js
@@ -5,9 +5,9 @@ $(function(){
       type: $tweetForm.attr('method'),
       url: $tweetForm.attr('action'),
       data: $tweetForm.serialize(),
-      success: function (data) {
-          alert('success');
+      success: function (data) {          
           $("#tweet").val('');
+          $(document).trigger("tweet:posted")
       },
       error: function(data) {
         alert(data.responseText)
